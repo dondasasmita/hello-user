0a5a3b016913ef80f8d83b2033bffe9107917b94
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-04-12 14:59:45 +0800

    upgraded to form handling

--- src/main/java/hello_user/GreetingController.java
+++ src/main/java/hello_user/GreetingController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.jws.WebParam;
@@ -13,25 +15,45 @@ import java.util.ArrayList;
 @Controller
 public class GreetingController {
 
-    User newUser = new User();
-
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=true) String name,
-                           @RequestParam(name = "age", required = true) int age,
-                           @RequestParam(name = "address", required = true) String address, Model model) {
-        newUser.setName(name);
-        newUser.setAge(age);
-        newUser.setAddress(address);
-        model.addAttribute("name", name);
+    public String greetingForm(Model model){
+        model.addAttribute("user", new User());
         return "greeting";
     }
 
-    @GetMapping("/user-info")
-    public String userInfo(Model model) {
-        model.addAttribute("age",newUser.getAge());
-        model.addAttribute("address",newUser.getAddress());
+    @PostMapping("/greeting")
+    public String userSubmit(@ModelAttribute User user) {
         return "user-info";
-
     }
+
+
+
+
+//    @GetMapping("/user-info")
+//    public String userInfo(Model model){
+//        model.addAttribute("user", new User());
+//        return "/user-info";
+//    }
+
+//    @GetMapping("/greeting")
+//    public String greeting(@RequestParam(name="name", required=true) String name,
+//                           @RequestParam(name = "age", required = true) int age,
+//                           @RequestParam(name = "address", required = true) String address, Model model) {
+//        newUser.setName(name);
+//        newUser.setAge(age);
+//        newUser.setAddress(address);
+//        model.addAttribute("name", name);
+//        return "greeting";
+//    }
+//
+//    @GetMapping("/user-info")
+//    public String userInfo(Model model) {
+//        model.addAttribute("age",newUser.getAge());
+//        model.addAttribute("address",newUser.getAddress());
+//        return "user-info";
+//
+//    }
+
+
 
 }
